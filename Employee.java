add5a48f64a18c6dd57ac26d0323bbf26b541a90
Ravi Mahajan <ravidnya@gmail.com>
2025-08-16 15:56:21 -0500

    test commit

--- Employee.java
+++ Employee.java
@@ -1,5 +1,7 @@
 class Employee {
   boolean status = false;
   int a = 10;
+
+  int b = 20;
 }
 
--- User.java
+++ User.java
@@ -4,4 +4,7 @@ class User {
 
     String name = "java";
     int c = 30;
+
+    int i = 10;
+    int j = 20;
 }
